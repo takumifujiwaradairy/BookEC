a496a3589fb54d707c34025a86c24865b00d8109
Your Name <you@example.com>
2022-03-04 13:18:30 +0900

    トップページの作成

---
+++ FrontBook/components/book/BookDefalut.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <book-index/>
+  </div>
+</template>
+
+<script>
+import BookIndex from './BookIndex.vue'
+export default {
+  components: { BookIndex },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ FrontBook/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <book-defalut/>
+</template>
+
+<script>
+import BookDefalut from '../components/book/BookDefalut.vue'
+export default {
+  components: { BookDefalut },
+  name: 'IndexPage'
+}
+</script>
