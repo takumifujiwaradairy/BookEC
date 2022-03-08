84b299846003f6201ac8445a64045764c888095e
Your Name <you@example.com>
2022-03-08 09:47:57 +0900

    カテゴリー検索機能

--- FrontBook/components/book/BooksList.vue
+++ FrontBook/components/book/BooksList.vue
@@ -14,7 +14,18 @@
         <label>出版日</label>
         <input type="date" v-model="query.release_day_lteq">
         <input type="date" v-model="query.release_day_gteq">
-        
+      </li>
+      <li>
+        <label>親のカテゴリー</label>
+        <select v-model="query.parentCategoryId" @change="parentCategoriesIsSelected()">
+          <option v-for="category in parentCategories()" :key="category.id" :value="category.id">{{ category.name }}</option>
+        </select>
+      </li>
+      <li>
+        <label>子のカテゴリー</label>
+        <select v-model="query.categories_id_in" >
+          <option v-for="category in childCategories(query.parentCategoryId)" :key="category.id" :value="parentCategoryId">{{ category.name }}</option>
+        </select>
       </li>
       <button @click="search">検索</button>
     </ul>
@@ -46,21 +57,29 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import axios from "axios";
 import Qs from "qs";
 export default {
   data() {
     return {
+      isSearch: false,
       query: {
         name_cont: '',
         author_cont: '',
         release_day_gteq: '',
         release_day_lteq: '',
+        categories_id_in: '',
+        parentCategoryId: '',
       },
     };
   },
   props: { 
     books: { 
+      type: Array,
+      required: true,
+    },
+    categories: {
       type: Array,
       required: true,
     }
@@ -75,6 +94,28 @@ export default {
       .then((response) => {
         this.$store.commit("setBooks", response.data);
       })
+    },
+    parentCategories() {
+      // parent_idが存在しないデータだけを絞り込む
+      const selectCategories = this.categories.filter(function(category){
+        return category.parent_id === null;
+      });
+      return selectCategories;
+    },
+    parentCategoriesIsSelected() {
+      this.isSearch = true;
+    },
+      // 親カテゴリーが選択されたidの物だけを絞り込むようにする。
+      // 親カテゴリーが選択されていない場合は、子カテゴリーをすべて表示する。
+    childCategories(parentCategoryId) {
+      if(this.isSearch) {
+        const selectCategories = this.categories.filter(function(category){
+          return category.parent_id === parentCategoryId;
+        })
+        return selectCategories;
+      } else {
+        return this.categories;
+      }
     }
   }
 }
